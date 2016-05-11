87ee9e07dfdd2fe5350cbace96fba7d6f1f8204b
arcnmx <arcnmx@users.noreply.github.com>
2016-05-11 12:16:42 -0400

    Always use https for fetch

--- bin/download.sh
+++ bin/download.sh
@@ -8,18 +8,16 @@ GIT_URL="https://github.com/arcnmx/dotfiles.git"
 
 OPT_ROOT=
 OPT_SETUP_TYPE=
-OPT_HTTPS_REMOTE=
 
 print_usage() {
 	echo "Usage: $0 [-r] [-s SETUP_TYPE]" >&2
 	echo "    -h: print this help" >&2
 	echo "    -r: install in /.dotfiles as root" >&2
 	echo "    -s SETUP_TYPE: initiate system setup as the specified system TYPE" >&2
-	echo "    -H: set the git remote url to https" >&2
 	exit 1
 }
 
-while getopts ":hHrs:" opt; do
+while getopts ":hrs:" opt; do
 	case "$opt" in
 		h) # --help
 			print_usage
@@ -30,9 +28,6 @@ while getopts ":hHrs:" opt; do
 			;;
 		s) # --setup TYPE
 			OPT_SETUP_TYPE="$OPTARG"
-			;;
-		H) # --https-remote
-			OPT_HTTPS_REMOTE=y
 			;;
 		\?)
 			echo "Invalid option -$OPTARG" >&2
@@ -58,9 +53,7 @@ if [ ! -e "$DOTFILES_PATH" ]; then
 fi
 cd "$DOTFILES_PATH"
 ./dot.sh crypt-unlock
-if [ -z "$OPT_HTTPS_REMOTE" ]; then
-	git remote set-url origin git@github.com:arcnmx/dotfiles.git
-fi
+git remote set-url --push origin git@github.com:arcnmx/dotfiles.git
 if [ -n "$OPT_SETUP_TYPE" ]; then
 	./dot.sh setup "$OPT_SETUP_TYPE"
 	./dot.sh update-system
