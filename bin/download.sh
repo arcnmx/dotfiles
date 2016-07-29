769e36dae86e02614a839fcfbc48238c181eaef1
arcnmx <arcnmx@users.noreply.github.com>
2016-07-29 17:32:26 -0400

    skip crypt option

--- bin/download.sh
+++ bin/download.sh
@@ -8,16 +8,18 @@ GIT_URL="https://github.com/arcnmx/dotfiles.git"
 
 OPT_ROOT=
 OPT_SETUP_TYPE=
+OPT_NO_CRYPT=
 
 print_usage() {
-	echo "Usage: $0 [-r] [-s SETUP_TYPE]" >&2
+	echo "Usage: $0 [-rC] [-s SETUP_TYPE]" >&2
 	echo "    -h: print this help" >&2
 	echo "    -r: install in /.dotfiles as root" >&2
 	echo "    -s SETUP_TYPE: initiate system setup as the specified system TYPE" >&2
+	echo "    -C: don't decrypt data" >&2
 	exit 1
 }
 
-while getopts ":hrs:" opt; do
+while getopts ":hrCs:" opt; do
 	case "$opt" in
 		h) # --help
 			print_usage
@@ -29,17 +31,26 @@ while getopts ":hrs:" opt; do
 		s) # --setup TYPE
 			OPT_SETUP_TYPE="$OPTARG"
 			;;
+		C) # --no-crypt
+			OPT_NO_CRYPT=y
+			;;
 		\?)
 			echo "Invalid option -$OPTARG" >&2
-			print_usage
+			print_usage >&2
 			;;
 		:)
 			echo "Option -$OPTARG requires an argument" >&2
-			print_usage
+			print_usage >&2
 			;;
 	esac
 done
 shift $((OPTIND-1))
+
+crypt_unlock() {
+	if [ -n "$OPT_NO_CRYPT" ]; then
+		./dot.sh crypt-unlock
+	fi
+}
 
 DOTFILES_PATH="$HOME/.dotfiles"
 
@@ -52,11 +63,11 @@ if [ ! -e "$DOTFILES_PATH" ]; then
 	git clone "$GIT_URL" "$DOTFILES_PATH"
 fi
 cd "$DOTFILES_PATH"
-./dot.sh crypt-unlock
+crypt_unlock
 git remote set-url --push origin git@github.com:arcnmx/dotfiles.git
 if [ -n "$OPT_SETUP_TYPE" ]; then
 	./dot.sh setup "$OPT_SETUP_TYPE"
 	./dot.sh update-system
-	./dot.sh crypt-unlock # we maybe just installed a system-wide git-crypt
+	crypt_unlock # we maybe just installed a system-wide git-crypt
 fi
 ./dot.sh update
