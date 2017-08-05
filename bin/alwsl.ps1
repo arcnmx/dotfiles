d42bf6bbcb594aff8164500e00e142c15bdd566e
arcnmx <arcnmx@users.noreply.github.com>
2017-08-05 19:47:52 -0400

    more windows

--- bin/alwsl.ps1
+++ bin/alwsl.ps1
@@ -1,4 +1,5 @@
 (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/alwsl/alwsl/master/alwsl.bat') -replace "`n", "`r`n" | Out-File -Encoding ASCII alwsl.bat
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 .\alwsl.bat install
 bash -c 'pacman --noconfirm -Syuw && rm /etc/ssl/certs/ca-certificates.crt && update-ca-trust && pacman --noconfirm -Su'
 bash -c 'pacman --noconfirm -S git curl'
