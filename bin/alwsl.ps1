c31dfed95018df5aaa4a96a45bcabe45f44c1728
arcnmx <arcnmx@users.noreply.github.com>
2017-08-05 19:28:27 -0400

    missing quotes

--- bin/alwsl.ps1
+++ bin/alwsl.ps1
@@ -1,4 +1,4 @@
-(New-Object System.Net.WebClient).DownloadString(https://raw.githubusercontent.com/alwsl/alwsl/master/alwsl.bat) | Out-File alwsl.bat
+(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/alwsl/alwsl/master/alwsl.bat') | Out-File alwsl.bat
 alwsl.bat # install
 bash -c 'pacman --noconfirm -Syuw && rm /etc/ssl/certs/ca-certificates.crt && update-ca-trust && pacman --noconfirm -Su'
 bash -c 'pacman --noconfirm -S git curl'
