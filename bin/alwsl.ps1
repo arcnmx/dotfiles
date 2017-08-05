6a31a1916b59be0be11195b33bcae02e2da4e6e1
arcnmx <arcnmx@users.noreply.github.com>
2017-08-05 19:41:17 -0400

    ugh windows

--- bin/alwsl.ps1
+++ bin/alwsl.ps1
@@ -1,7 +1,7 @@
-(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/alwsl/alwsl/master/alwsl.bat') | Out-File alwsl.bat
-alwsl.bat # install
+(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/alwsl/alwsl/master/alwsl.bat') -replace "`n", "`r`n" | Out-File -Encoding ASCII alwsl.bat
+.\alwsl.bat install
 bash -c 'pacman --noconfirm -Syuw && rm /etc/ssl/certs/ca-certificates.crt && update-ca-trust && pacman --noconfirm -Su'
 bash -c 'pacman --noconfirm -S git curl'
 bash -c 'curl -sSfL https://raw.githubusercontent.com/arcnmx/dotfiles/master/bin/download.sh | sh -s -- -r -s personal'
 bash -c '/.dotfiles/dot.sh newuser arc wheel'
-alwsl.bat default arc
+.\alwsl.bat default arc
