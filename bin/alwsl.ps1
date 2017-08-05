a5da07dda735c23672be371d2c290b6d999a3d59
arcnmx <arcnmx@users.noreply.github.com>
2017-08-05 19:52:58 -0400

    windows sudo!

--- bin/alwsl.ps1
+++ bin/alwsl.ps1
@@ -1,5 +1,5 @@
 (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/alwsl/alwsl/master/alwsl.bat') -replace "`n", "`r`n" | Out-File -Encoding ASCII alwsl.bat
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+Start-Process -Wait -Verb RunAs -ArgumentList "-Command","Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux" powershell
 .\alwsl.bat install
 bash -c 'pacman --noconfirm -Syuw && rm /etc/ssl/certs/ca-certificates.crt && update-ca-trust && pacman --noconfirm -Su'
 bash -c 'pacman --noconfirm -S git curl'
