bf9667f590c70a8da12f05cae0282edd56f4e849
arcnmx <arcnmx@users.noreply.github.com>
2017-08-05 20:44:20 -0400

    wsl bash must be run as login shell

--- bin/alwsl.ps1
+++ bin/alwsl.ps1
@@ -9,8 +9,8 @@ Start-Process -Wait -Verb RunAs -ArgumentList "-Command","Enable-WindowsOptional
 # Install Arch using https://github.com/alwsl/alwsl
 (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/alwsl/alwsl/master/alwsl.bat') -replace "`n", "`r`n" | Out-File -Encoding ASCII alwsl.bat
 .\alwsl.bat install
-bash -c 'pacman --noconfirm -Syu'
-bash -c 'pacman --noconfirm -S git curl'
-bash -c 'curl -sSfL https://raw.githubusercontent.com/arcnmx/dotfiles/master/bin/download.sh | sh -s -- -r -s personal'
-bash -c '/.dotfiles/dot.sh newuser arc wheel'
+bash -lc 'pacman --noconfirm -Syu'
+bash -lc 'pacman --noconfirm -S git curl'
+bash -lc 'curl -sSfL https://raw.githubusercontent.com/arcnmx/dotfiles/master/bin/download.sh | sh -s -- -r -s personal'
+bash -lc '/.dotfiles/dot.sh newuser arc wheel'
 .\alwsl.bat user default arc
