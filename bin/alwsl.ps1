6084e469c6883660ba64a46436756e9c8673066f
arcnmx <arcnmx@users.noreply.github.com>
2017-10-16 19:33:06 -0400

    useless alwsl updates

--- bin/alwsl.ps1
+++ bin/alwsl.ps1
@@ -9,10 +9,15 @@ Start-Process -Wait -Verb RunAs -ArgumentList "add","HKEY_LOCAL_MACHINE\SOFTWARE
 Start-Process -Wait -Verb RunAs -ArgumentList "-Command","Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux" powershell
 
 # Install Arch using https://github.com/alwsl/alwsl
-(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/alwsl/alwsl/master/alwsl.bat') -replace "`n", "`r`n" | Out-File -Encoding ASCII alwsl.bat
-.\alwsl.bat install
+$ALWSL = "$env:TEMP\alwsl.bat"
+(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/alwsl/alwsl/master/alwsl.bat') -replace "`n", "`r`n" | Out-File -Encoding ASCII $ALWSL
+& $ALWSL install
+
+bash -lc 'pacman --noconfirm -Sy pacman'
 bash -lc 'pacman --noconfirm -Syu'
 bash -lc 'pacman --noconfirm -S git curl'
+
 bash -lc 'curl -sSfL https://raw.githubusercontent.com/arcnmx/dotfiles/master/bin/download.sh | sh -s -- -r -s personal'
+
 bash -lc '/.dotfiles/dot.sh newuser arc wheel'
-.\alwsl.bat user default arc
+& $ALWSL user default arc
