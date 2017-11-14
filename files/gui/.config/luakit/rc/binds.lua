fbf1cb16d36749f6b495432fd2cdd1ae3b422a45
arcnmx <arcnmx@users.noreply.github.com>
2017-11-14 10:13:07 -0500

    luakit ;m bind

--- files/gui/.config/luakit/rc/binds.lua
+++ files/gui/.config/luakit/rc/binds.lua
@@ -20,9 +20,28 @@ modes.remove_binds("normal", { "<control-w>" })
 
 modes.add_cmds({
     { ":mpv", "Open current URL in mpv",
-        function (w) os.execute(string.format("mpv --keep-open=yes %q &", w.view.uri)) end },
+        function (w)
+            os.execute(string.format("mpv --keep-open=yes %q &", w.view.uri))
+            w:notify("Playing: " .. w.view.uri)
+        end },
     { ":mpa", "Open current URL in mpv (audio only)",
-        function (w) os.execute(string.format("urxvt -e mpv --no-video %q &", w.view.uri)) end },
+        function (w)
+            os.execute(string.format("urxvt -e mpv --no-video %q &", w.view.uri))
+            w:notify("Playing: " .. w.view.uri)
+        end },
+})
+
+modes.add_binds("ex-follow", {
+    { "m", "Hint all links and play matched element with mpv",
+        function (w)
+            w:set_mode("follow", {
+                prompt = "yank", selector = "uri", evaluator = "uri",
+                func = function (uri)
+                    os.execute(string.format("mpv --keep-open=yes %q &", uri))
+                    w:notify("Playing: " .. uri)
+                end
+            })
+        end },
 })
 
 -- vim: et:sw=4:ts=8:sts=4:tw=80
