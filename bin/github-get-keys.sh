1946365266e965aeea1bbb36efde6c37df26aa14
arcnmx <arcnmx@users.noreply.github.com>
2017-12-12 13:48:06 -0500

    improve github scripts

--- bin/github-get-keys.sh
+++ bin/github-get-keys.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 set -eu
 
-if [ "$#" -lt 1 ]; then
+if [[ "$#" -lt 1 ]]; then
 	echo "Usage: $0 GITHUB_USERNAME [GITHUB_PASSWORD]" >&2
 	exit 1
 fi
 
-USERNAME="$1"
-
-if [ "$#" -lt 2 ]; then
-	echo -n "Enter github password for $USERNAME: " >&2
-	read -rs PASSWORD
-	echo >&2
-else
-	PASSWORD="$2"
+if [[ ! -t 0 ]]; then
+	exec < /dev/tty
 fi
 
-curl -sSf -u "$USERNAME":"$PASSWORD" "https://api.github.com/user/keys" | grep '"key"' | grep -o 'ssh-[^"]*'
+USERNAME="$1"
+
+if [[ "$#" -lt 2 ]]; then
+	PASSWORD=$(pass "$USERNAME/github.com" 2> /dev/null || true)
+
+	if [[ -z "$PASSWORD" ]]; then
+		echo -n "Enter github password for $USERNAME: " >&2
+		read -rs PASSWORD
+		echo >&2
+
+		if [[ -z "$PASSWORD" ]]; then
+			exit 1
+		fi
+
+		ARGS=(-u "$USERNAME:$PASSWORD")
+	else
+		ARGS=(-u "$USERNAME:$(echo "$PASSWORD" | head -n 1)")
+		if echo "$PASSWORD" | grep -q ^otpauth://; then
+			ARGS+=(-H "X-GitHub-OTP: $(pass otp "$USERNAME/github.com")")
+		fi
+	fi
+else
+	ARGS=(-u "$USERNAME:$2")
+fi
+
+curl -sSf "${ARGS[@]}" "https://api.github.com/user/keys" | grep '"key"' | grep -o 'ssh-[^"]*'
--- bin/github-upload-key.sh
+++ bin/github-upload-key.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 set -eu
 
-if [ "$#" -lt 2 ]; then
+if [[ "$#" -lt 2 ]]; then
 	echo "Usage: $0 PUBKEY_PATH GITHUB_USERNAME [GITHUB_PASSWORD]" >&2
 	exit 1
+fi
+
+if [[ ! -t 0 ]]; then
+	exec < /dev/tty
 fi
 
 KEYFILE="$1"
 USERNAME="$2"
 
-if [ "$#" -lt 3 ]; then
-	echo -n "Enter github password for $USERNAME: " >&2
-	read -rs PASSWORD
-	echo >&2
+if [[ "$#" -lt 3 ]]; then
+	PASSWORD=$(pass "$USERNAME/github.com" 2> /dev/null || true)
+
+	if [[ -z "$PASSWORD" ]]; then
+		echo -n "Enter github password for $USERNAME: " >&2
+		read -rs PASSWORD
+		echo >&2
+
+		if [[ -z "$PASSWORD" ]]; then
+			exit 1
+		fi
+
+		ARGS=(-u "$USERNAME:$PASSWORD")
+	else
+		ARGS=(-u "$USERNAME:$(echo "$PASSWORD" | head -n 1)")
+		if echo "$PASSWORD" | grep -q ^otpauth://; then
+			ARGS+=(-H "X-GitHub-OTP: $(pass otp "$USERNAME/github.com")")
+		fi
+	fi
 else
-	PASSWORD="$3"
+	ARGS=(-u "$USERNAME:$3")
 fi
 
 KEYDATA=$(cat "$KEYFILE")
@@ -22,4 +41,4 @@ KEYCOMMENT=$(echo "$KEYDATA" | cut -d ' ' -f 3-)
 
 POST_DATA=$(printf '{ "title": "%s", "key": "%s" }' "$KEYCOMMENT" "$KEYDATA")
 
-curl -sSf -u "$USERNAME":"$PASSWORD" -d "$POST_DATA" "https://api.github.com/user/keys" > /dev/null
+curl -sSf "${ARGS[@]}" -d "$POST_DATA" "https://api.github.com/user/keys" > /dev/null
